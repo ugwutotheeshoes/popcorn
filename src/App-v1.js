23a5e01c1f797689f15468ee60b709fb55242e26
Emmanuel Ugwu <>
2023-11-27 08:05:01 +0100

    Update App-v1.js

--- src/App-v1.js
+++ src/App-v1.js
@@ -87,7 +87,7 @@ export default function App() {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok)
@@ -330,7 +330,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     function () {
       async function getMovieDetails() {
         const res = await fetch(
-          `http://www.omdbapi.com/?&apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?&apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
