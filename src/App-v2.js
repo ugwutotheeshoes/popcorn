adbeabc031725f7060ea81b7ad3bebfd431edb35
Emmanuel Ugwu <>
2023-12-01 16:59:10 +0100

    Update App-v2.js

--- src/App-v2.js
+++ src/App-v2.js
@@ -291,7 +291,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       document.title = `Movie | ${title}`;
 
       return function () {
-        document.title = "usePopcorn";
+        document.title = "PoppedCorn";
         // console.log(`Clean up effect for movie ${title}`);
       };
     },
